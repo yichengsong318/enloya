583a7cb2fc28c66c52b3b0e47e742ac255dc7f9a
Hakim Ouake <hakimouake@gmail.com>
2020-04-13 13:36:25 +0100

    Change a in Link.

--- src/pagers/account-settings/FixFees.jsx
+++ src/pagers/account-settings/FixFees.jsx
@@ -1,7 +1,7 @@
 import React, {Component} from 'react';
 import { connect } from "react-redux";
 import { readData } from "../../redux/actions";
-import { withRouter } from "react-router-dom";
+import { withRouter, Link } from "react-router-dom";
 import FixedServiceCard from "../../shared/FixedServiceCard";
 
 
@@ -16,7 +16,8 @@ export class FixFees extends Component {
       <div className="py-4 px-2 account-settings">
         <h2 className="mt-2 mb-3 pl-4">Fix-fee Services</h2>
         <div className="pb-4 text-right">
-          <a href="/account-settings/fix-fee-services-create" className="btn btn-primary px-5">Create a new service</a>
+          <Link to="/account-settings/fix-fee-services-create" 
+            className="btn btn-primary px-5">Create a new service</Link>
         </div>
         <div className="py-4 fixed-services">
           <div className="row mx-auto">
