c0c006286eec10db71dc9a941c605e5dde4395ef
win <fancodeexpert@outlook.com>
2020-05-29 14:05:26 +0100

    feat: add notification when public link doesn't match specified criterias

--- src/shared/FormElement.jsx
+++ src/shared/FormElement.jsx
@@ -70,15 +70,30 @@ export function FormTag(props) {
 }
 
 export function FormInput(props) {
+  const publicLink = React.createRef();
+  const handleMouseOver =()=>{
+    publicLink.current.classList.remove('d-none');
+  }
+
+  const handleMouseOut =()=>{
+    publicLink.current.classList.add('d-none');
+  }
+
   return (
     <div className={"form-group text-left" + (props.customClass ? ' ' + props.customClass : '')}>
       {!props.noLabel && <label htmlFor={props.id}>{props.label}</label>}
-       {props.showAlert &&(<span className="text-danger">(should be 70 characters maximum)</span>)}
+       {props.showAlert &&(<div className="danger-zone">must contain 3-100 letters or numbers. Please do not use spaces, symbols, or special characters.</div>)}
       <div className="d-flex">
-        <input type={props.type} onChange={e => props.onChange(props.name, e.target.value)} id={props.id} 
-          className="form-control" maxLength={props.maxLength ? props.maxLength : 300}
+        <input type={props.type} onChange={e => props.onChange(props.name, e.target.value)} id={props.id}
+          maxLength={props.maxLength ? props.maxLength : 300}
+          className="form-control"
           value={props.value} placeholder={props.placeholder} />
-        {!props.noHelp && <FontAwesomeIcon icon={faQuestion} className="form-question-icon" />}
+        {!props.noHelp && <
+          FontAwesomeIcon icon={faQuestion} className="form-question-icon"
+            onMouseOver={() => handleMouseOver()}
+            onMouseOut={() => handleMouseOut()}
+          />}
+        {props.message && <span ref={publicLink} className="d-none position-absolute warning-message">{props.message}</span>}
       </div>
     </div>
   );
